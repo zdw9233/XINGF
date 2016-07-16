064f98814bc797bf1200e17b663324aeb4eb2782
leeiidesu <136306413@qq.com>
2016-07-16 19:16:09 +0800

    hahahha

--- youyiCustom/app/src/main/java/com/uyi/app/ui/common/model/AbnormalEvent.java
+++ youyiCustom/app/src/main/java/com/uyi/app/ui/common/model/AbnormalEvent.java
@@ -4,4 +4,15 @@ package com.uyi.app.ui.common.model;
  * Created by Leeii on 2016/7/16.
  */
 public class AbnormalEvent {
+    /**
+     * id : 1
+     * name : 脑血拴
+     * eventType : 2
+     * description : 头疼，脑热
+     */
+
+    public int id;
+    public String name;
+    public int eventType;
+    public String description;
 }
