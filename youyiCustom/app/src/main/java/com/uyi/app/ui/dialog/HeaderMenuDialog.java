0385c3768f3b585736f0e615f757e1fc09d5764f
leeiidesu <136306413@qq.com>
2016-07-21 10:18:02 +0800

    as

--- youyiCustom/app/src/main/java/com/uyi/app/ui/dialog/HeaderMenuDialog.java
+++ youyiCustom/app/src/main/java/com/uyi/app/ui/dialog/HeaderMenuDialog.java
@@ -1,8 +1,10 @@
 package com.uyi.app.ui.dialog;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
@@ -14,7 +16,6 @@ import com.uyi.app.UserInfoManager;
 import com.uyi.app.model.bean.UserInfo;
 import com.uyi.app.service.MessageService;
 import com.uyi.app.ui.common.LoginActivity;
-import com.uyi.app.ui.dialog.MessageConform.MessageType;
 import com.uyi.app.ui.dialog.MessageConform.OnMessageClick;
 import com.uyi.app.ui.dialog.MessageConform.Result;
 import com.uyi.app.ui.personal.message.MessageActivity;
@@ -24,143 +25,149 @@ import com.uyi.custom.app.R;
 
 /**
  * 点击头像出来的菜单
- * @author user
  *
+ * @author user
  */
 public class HeaderMenuDialog extends AbstrctDialog implements android.view.View.OnClickListener, OnMessageClick {
 
-	Context context; 
-	private LinearLayout header_menu_dou; 
-	private TextView header_menu_dou_count; 
-	
-	private LinearLayout header_menu_message; 
-	private TextView header_meun_message_count; 
-	private TextView header_meun_zx_count; 
-	private TextView header_meun_rc_count; 
-	
-	private LinearLayout header_menu_zixun; 
-	private LinearLayout header_menu_richeng; 
-	private LinearLayout header_menu_exit; 
-	
-	private MessageConform conform;
-	
-	public HeaderMenuDialog(Context context) {
-		super(context,R.style.dialog);
-	}
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		requestWindowFeature(Window.FEATURE_NO_TITLE);  
-		Window dialogWindow = getWindow();
-		dialogWindow.setGravity(Gravity.LEFT | Gravity.TOP);
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.header_menu_dialog);
-		header_menu_dou = $( R.id.header_menu_dou);
-		header_menu_dou_count = $( R.id.header_menu_dou_count);
-		header_menu_message = $( R.id.header_menu_message);
-		header_meun_message_count = $( R.id.header_meun_message_count);
-		header_meun_zx_count = $( R.id.header_meun_zx_count);
-		header_meun_rc_count = $( R.id.header_meun_rc_count);
-		header_menu_zixun = $( R.id.header_menu_zixun);
-		header_menu_richeng = $( R.id.header_menu_richeng);
-		header_menu_exit = $( R.id.header_menu_exit);
-		header_menu_exit.setOnClickListener(this);
-		header_menu_zixun.setOnClickListener(this);
-		header_menu_richeng.setOnClickListener(this);
-		header_menu_message.setOnClickListener(this);
-	}
+    Context context;
+    private LinearLayout header_menu_dou;
+    private TextView header_menu_dou_count;
 
-	public LinearLayout getHeader_menu_dou() {
-		return header_menu_dou;
-	} 
+    private LinearLayout header_menu_message;
+    private TextView header_meun_message_count;
+    private TextView header_meun_zx_count;
+    private TextView header_meun_rc_count;
 
-	public TextView getHeader_menu_dou_count() {
-		return header_menu_dou_count;
-	} 
-	public LinearLayout getHeader_menu_message() {
-		return header_menu_message;
-	}
- 
-	public TextView getHeader_meun_message_count() {
-		return header_meun_message_count;
-	}
- 
+    private LinearLayout header_menu_zixun;
+    private LinearLayout header_menu_richeng;
+    private LinearLayout header_menu_exit;
 
-	public LinearLayout getHeader_menu_zixun() {
-		return header_menu_zixun;
-	}
- 
+    private MessageConform conform;
 
-	public LinearLayout getHeader_menu_richeng() {
-		return header_menu_richeng;
-	}
- 
+    public HeaderMenuDialog(Context context) {
+        super(context, R.style.dialog);
+    }
 
-	public LinearLayout getHeader_menu_exit() {
-		return header_menu_exit;
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        Window dialogWindow = getWindow();
+        dialogWindow.setGravity(Gravity.LEFT | Gravity.TOP);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.header_menu_dialog);
+        header_menu_dou = $(R.id.header_menu_dou);
+        header_menu_dou_count = $(R.id.header_menu_dou_count);
+        header_menu_message = $(R.id.header_menu_message);
+        header_meun_message_count = $(R.id.header_meun_message_count);
+        header_meun_zx_count = $(R.id.header_meun_zx_count);
+        header_meun_rc_count = $(R.id.header_meun_rc_count);
+        header_menu_zixun = $(R.id.header_menu_zixun);
+        header_menu_richeng = $(R.id.header_menu_richeng);
+        header_menu_exit = $(R.id.header_menu_exit);
+        header_menu_exit.setOnClickListener(this);
+        header_menu_zixun.setOnClickListener(this);
+        header_menu_richeng.setOnClickListener(this);
+        header_menu_message.setOnClickListener(this);
+    }
 
-	@Override
-	public void onClick(View v) {
-		if(v.getId() == R.id.header_menu_message){
-			getContext().startActivity(new Intent(getContext(), MessageActivity.class));
-			
-		}else if(v.getId() == R.id.header_menu_zixun){
+    public LinearLayout getHeader_menu_dou() {
+        return header_menu_dou;
+    }
+
+    public TextView getHeader_menu_dou_count() {
+        return header_menu_dou_count;
+    }
+
+    public LinearLayout getHeader_menu_message() {
+        return header_menu_message;
+    }
+
+    public TextView getHeader_meun_message_count() {
+        return header_meun_message_count;
+    }
+
+
+    public LinearLayout getHeader_menu_zixun() {
+        return header_menu_zixun;
+    }
+
+
+    public LinearLayout getHeader_menu_richeng() {
+        return header_menu_richeng;
+    }
+
+
+    public LinearLayout getHeader_menu_exit() {
+        return header_menu_exit;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.header_menu_message) {
+            getContext().startActivity(new Intent(getContext(), MessageActivity.class));
+
+        } else if (v.getId() == R.id.header_menu_zixun) {
 //			Intent intent = new Intent(getContext(), HealthyQuestionsActivity.class);
 //			getContext().startActivity(intent);
-		}else if(v.getId() == R.id.header_menu_richeng){
-			getContext().startActivity(new Intent(getContext(), ScheduleActivity.class));
-		}else if(v.getId() == R.id.header_menu_exit){
-			if(conform == null){
-				conform = new MessageConform(getContext(), MessageType.CONFORM);
-			}
-			conform.setTitle("提示").setContent("确认要退出吗?").setOnMessageClick(this);
-			conform.show();
-		}
-		dismiss();
-	}
+        } else if (v.getId() == R.id.header_menu_richeng) {
+            getContext().startActivity(new Intent(getContext(), ScheduleActivity.class));
+        } else if (v.getId() == R.id.header_menu_exit) {
+//			if(conform == null){
+//				conform = new MessageConform(getContext(), MessageType.CONFORM);
+//			}
+//			conform.setTitle("提示").setContent("确认要退出吗?").setOnMessageClick(this);
+//			conform.show();
+            new AlertDialog.Builder(getContext()).setTitle("提示").setMessage("确认要退出吗?").setPositiveButton("确定", new OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    UserInfoManager.clearLoginUserInfo(getContext());
+                    getContext().startActivity(new Intent(getContext(), LoginActivity.class));
+                }
+            }).setNegativeButton("取消", null).show();
+        }
+        dismiss();
+    }
 
-	@Override
-	public void onClick(Result result, Object object) {
-		if(result == Result.OK){
+    @Override
+    public void onClick(Result result, Object object) {
+        if (result == Result.OK) {
+            Log.e("====================", "login");
+            getContext().startActivity(new Intent(getContext(), LoginActivity.class));
+            UserInfoManager.clearLoginUserInfo(getContext());
+            Log.e("====================", "logi22n");
+        }
+    }
 
-			Log.e("====================","login");
-			getContext().startActivity(new Intent(getContext(), LoginActivity.class));
-			UserInfoManager.clearLoginUserInfo(getContext());
-			Log.e("====================","logi22n");
-		}
-	}
- 
 
-	
-	@Override
-	public void show() {
-		super.show();
+    @Override
+    public void show() {
+        super.show();
 //		MessageService.loadMessagesAll(context);
-		int count = MessageService.getMessageCount(getContext(), MessageService.IM);
-		if(count == 0){
-			header_meun_message_count.setVisibility(View.INVISIBLE);
-		}else{
-			header_meun_message_count.setVisibility(View.VISIBLE);
-			header_meun_message_count.setText(count+"");
-		}
-		count = MessageService.getMessageCount(getContext(), MessageService.ZX);
-		if(count == 0){
-			header_meun_zx_count.setVisibility(View.INVISIBLE);
-		}else{
-			header_meun_zx_count.setVisibility(View.VISIBLE);
-			header_meun_zx_count.setText(count+"");
-		}
-		count = MessageService.getMessageCount(getContext(), MessageService.RC);
-		if(count == 0){
-			header_meun_rc_count.setVisibility(View.INVISIBLE);
-		}else{
-			header_meun_rc_count.setVisibility(View.VISIBLE);
-			header_meun_rc_count.setText(count+"");
-		}
-		UserInfo info = UserInfoManager.getLoginUserInfo(getContext());
-		if(info != null && info.beans > 0){
-			header_menu_dou_count.setText(info.beans+"");
-		}
-	}
+        int count = MessageService.getMessageCount(getContext(), MessageService.IM);
+        if (count == 0) {
+            header_meun_message_count.setVisibility(View.INVISIBLE);
+        } else {
+            header_meun_message_count.setVisibility(View.VISIBLE);
+            header_meun_message_count.setText(count + "");
+        }
+        count = MessageService.getMessageCount(getContext(), MessageService.ZX);
+        if (count == 0) {
+            header_meun_zx_count.setVisibility(View.INVISIBLE);
+        } else {
+            header_meun_zx_count.setVisibility(View.VISIBLE);
+            header_meun_zx_count.setText(count + "");
+        }
+        count = MessageService.getMessageCount(getContext(), MessageService.RC);
+        if (count == 0) {
+            header_meun_rc_count.setVisibility(View.INVISIBLE);
+        } else {
+            header_meun_rc_count.setVisibility(View.VISIBLE);
+            header_meun_rc_count.setText(count + "");
+        }
+        UserInfo info = UserInfoManager.getLoginUserInfo(getContext());
+        if (info != null && info.beans > 0) {
+            header_menu_dou_count.setText(info.beans + "");
+        }
+    }
 }
