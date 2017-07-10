8324b7a6c0410397b9562964c95cc07f5b33caad
zhaodawei <771260789@qq.com>
2017-07-10 15:24:13 +0800

    Rejected commit: Default

--- XINGF/app/src/main/java/com/uyi/xinf/ui/healthmanager/HealthExaminationActivity.java
+++ XINGF/app/src/main/java/com/uyi/xinf/ui/healthmanager/HealthExaminationActivity.java
@@ -1,5 +1,7 @@
 package com.uyi.xinf.ui.healthmanager;
 
+import com.lidroid.xutils.view.annotation.ContentView;
+import com.uyi.doctor.xinf.R;
 import com.uyi.xinf.ui.custom.BaseActivity;
 import com.uyi.xinf.ui.custom.SystemBarTintManager;
 
@@ -7,7 +9,7 @@ import com.uyi.xinf.ui.custom.SystemBarTintManager;
  * Created by ThinkPad on 2017/6/30.
  * 健康体检
  */
-
+@ContentView(R.layout.activity_health_examination)
 public class HealthExaminationActivity extends BaseActivity {
     @Override
     protected void onInitLayoutAfter() {
